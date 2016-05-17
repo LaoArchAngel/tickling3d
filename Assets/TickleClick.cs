32712bb3780854504d7bafebb013122dd8adace2
Malick McGregor <lao@nexverse.net>
2016-05-16 22:00:46 -0600

    Added TickleClick script. Possible animation issues.

---
+++ Assets/TickleClick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TickleClick : MonoBehaviour {
+	
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Construct a ray from the current mouse coordinates
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.tag != "Ticklish")
+                    return;
+                Animator anim = hit.collider.gameObject.GetComponent<Animator>();
+                anim.StartPlayback();
+                //Check which object was hit and do something
+            }
+        }
+    }
+}
